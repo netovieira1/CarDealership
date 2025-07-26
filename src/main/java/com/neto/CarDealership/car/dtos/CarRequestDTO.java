b77ae135e76d4655b79223409d30b7be73a03cfc
Neto Vieira <128878577+netovieira1@users.noreply.github.com>
2025-07-26 01:34:07 -0300

    feat: CarRequest criado para fazer alterações no updateById

---
+++ src/main/java/com/neto/CarDealership/car/dtos/CarRequestDTO.java
@@ -0,0 +1,64 @@
+package com.neto.CarDealership.car.dtos;
+
+public class CarRequestDTO {
+
+    private String name;
+
+    private String brand;
+
+    private String model;
+
+    private Integer carYear;
+
+    private String color;
+
+    private Long clientId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getCarYear() {
+        return carYear;
+    }
+
+    public void setCarYear(Integer carYear) {
+        this.carYear = carYear;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+}
