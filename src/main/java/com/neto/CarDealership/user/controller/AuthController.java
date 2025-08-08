0a31646820646c15a8439e3c71fd6b7d9590cb0d
Neto Vieira <128878577+netovieira1@users.noreply.github.com>
2025-08-08 01:57:43 -0300

    feat: Feito ajustes no AuthController e criação do UserController

---
+++ src/main/java/com/neto/CarDealership/user/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.neto.CarDealership.user.controller;
+
+
+import com.neto.CarDealership.user.dto.AuthRequest;
+import com.neto.CarDealership.user.dto.AuthResponse;
+import com.neto.CarDealership.user.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody AuthRequest request) {
+        authService.register(request);
+        return ResponseEntity.ok("Usuário registrado com  sucesso!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
--- src/main/java/com/neto/CarDealership/user/controller/UserController.java
+++ src/main/java/com/neto/CarDealership/user/controller/UserController.java
@@ -1,33 +1,55 @@
 package com.neto.CarDealership.user.controller;
 
+import com.neto.CarDealership.user.dto.UserDTO;
+import com.neto.CarDealership.user.dto.UserResponseDTO;
+import com.neto.CarDealership.user.mappers.UserMapper;
+import com.neto.CarDealership.user.model.UserModel;
 import com.neto.CarDealership.user.service.AuthService;
-import com.neto.CarDealership.user.dto.AuthRequest;
-import com.neto.CarDealership.user.dto.AuthResponse;
+import com.neto.CarDealership.user.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/user")
 public class UserController {
 
-    private final AuthService authService;
+    private final  UserService userService;
 
-    public UserController(AuthService authService) {
-        this.authService = authService;
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody AuthRequest request) {
-        authService.register(request);
-        return ResponseEntity.ok("Usuário registrado com  sucesso!");
+    @PostMapping("/create")
+    public ResponseEntity<UserResponseDTO> create(@RequestBody @Valid UserDTO userDTO){
+        UserResponseDTO userResponseDTO = userService.createUser(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDTO);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-        AuthResponse response = authService.login(request);
-        return ResponseEntity.ok(response);
+   @GetMapping("/all")
+        public ResponseEntity<List<UserResponseDTO>> getAll(){
+            List<UserResponseDTO> userResponseDTO = userService.getAll();
+            return ResponseEntity.ok(userResponseDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getById(@PathVariable Long id){
+        UserResponseDTO userResponseDTO  = userService.findById(id);
+        return ResponseEntity.ok(userResponseDTO);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<UserResponseDTO> updateById(@PathVariable Long id, @RequestBody UserDTO userDTO){
+        UserResponseDTO updatedUser = userService.updateById(id,userDTO);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
